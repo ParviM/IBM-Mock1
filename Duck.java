da75683436368c9339e4d00e74ec4088cfe427f6
ParviM <89266514+ParviM@users.noreply.github.com>
2021-08-23 12:22:44 +0530

    Add files via upload

---
+++ Duck.java
@@ -0,0 +1,26 @@
+package org.ibm.training;
+
+public class Duck extends Animal
+{
+   protected String beakColor;
+  public Duck( String beak_color,int Age, boolean Gender, boolean Mammal, boolean mate)
+  {
+      super(Age,Gender,Mammal,mate);
+       this.beakColor = beak_color;
+  }
+  public String GetBeakColor() { return(beakColor); }
+ public void  swim() { System.out.println("swimming...");  }
+ public void quack() {  System.out.println("quack"); }
+ @Override
+ public boolean mate() {
+	  return(mate);
+ }
+ @Override public String SerializeToCSV()
+ {
+       return(
+            new String (
+                        age + "," + gender + "," + mammal + "," + beakColor+","+mate
+                       )
+             );
+ }
+}
